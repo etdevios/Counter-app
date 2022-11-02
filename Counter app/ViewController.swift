c653d082d05aaba88875b08896fb03fad242c913
Eduard Tokarev <etdevios@gmail.com>
2022-11-02 09:59:37 +0300

    feat!: подготовлено и настроено приложение счётчик.

--- Counter app/ViewController.swift
+++ Counter app/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var countApp = 1
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func plusButton(_ sender: UIButton) {
+        countLabel.text = "Значение счётчика: \(countApp)"
+        countApp += 1
+    }
+    
 }
-
